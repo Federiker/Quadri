0db3a83f8d1e2523ba492db2eca8eb4e21f9d097
Federiker <federiker@outlook.com>
2015-06-12 17:08:08 +0200

    Added option to remove magnifier

--- web/maker.php
+++ web/maker.php
@@ -1,6 +1,9 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 define("CONT_W", 480);
+//define("CONT_W", 600);
+define("MAG_CSS_URL", "https://dry-journey-4380.herokuapp.com/magnifier.min.css");
+define("MAG_EMPTY_CSS_URL", "https://dry-journey-4380.herokuapp.com/empty.css");
 //http://www.federicopirani.com/Quadri/paintings/GK60900007.jpg
 //http://www.federicopirani.com/Quadri/paintings/guardians.jpg
 //http://upload.wikimedia.org/wikipedia/commons/d/d4/Paul_C%C3%A9zanne%2C_1892-95%2C_Les_joueurs_de_carte_%28The_Card_Players%29%2C_60_x_73_cm%2C_oil_on_canvas%2C_Courtauld_Institute_of_Art%2C_London.jpg
@@ -20,6 +23,7 @@ $ratio = 1;
 $norepet = !isset($_POST["repeat"]);
 $create = isset($_POST["create"]);
 $show3d = isset($_POST["show_3d"]);
+$magnify = isset($_POST["magnify"]);
 
 $options = new Options();
 try {
@@ -50,8 +54,9 @@ catch (Exception $e) {
 $zip = new ZipArchive();
 $zip->open("public/ebay_{$n}.zip", ZipArchive::CREATE);
 
-
+$cont_w = CONT_W + ($magnify ? 0 : 120);
 $thumb = null;
+
 try {
     $img = Intervention\Image\ImageManagerStatic::make($url);
     $w = $img->width();
@@ -61,14 +66,14 @@ try {
         $img->save("public/{$n}.jpg");
         $url = "public/{$n}.jpg";
         $zip->addFile($url, "{$n}.jpg");
-        $img->resize(CONT_W, CONT_W * $h / $w);
+        $img->resize($cont_w, $cont_w * $h / $w);
         $img->save("public/{$n}_small.jpg");
         $zip->addFile("public/{$n}_small.jpg", "{$n}_small.jpg");
         $thumb = "'public/{$n}_small.jpg'";
     }
 }
 catch (Exception $e) {
-    echo "<!--" . var_dump($e) . "-->";
+    //echo "<!--" . var_dump($e) . "-->";
 }
 
 $twig = new Twig_Environment($loader, array(
@@ -77,7 +82,14 @@ $twig = new Twig_Environment($loader, array(
 
 $parser = new Less_Parser(array("compress" => true));
 $parser->parseFile( 'style.less', '.' );
-$parser->ModifyVars( array('ratio'=>$ratio, "bck-size" => $bckSize, "thumb-width" => CONT_W) );
+$parser->ModifyVars(
+    array(
+        'ratio'=>$ratio, 
+        "bck-size" => $bckSize, 
+        "thumb-width" => $cont_w,
+        "mag-url" => $magnify ? MAG_CSS_URL : MAG_EMPTY_CSS_URL
+        )
+    );
 $css = $parser->getCss();
 $vals = array(
     "title" => $title,
@@ -89,14 +101,15 @@ $vals = array(
     "code" => $code,
     "img_url" => "'" . $url . "'",
     "img_thumb" => $thumb,
-    "EventScript" => file_get_contents("Event.min.js"),
-    "MagnifierScript" => file_get_contents("Magnifier.min.js"),
-    "PaintingScript" => file_get_contents("Paintings.min.js"),
-    "MainScript" => file_get_contents("mainscript.min.js"),
+    "EventScript" => $magnify ? file_get_contents("Event.min.js") : "",
+    "MagnifierScript" => $magnify ? file_get_contents("Magnifier.min.js") : "",
+    "PaintingScript" => $magnify ? file_get_contents("Paintings.min.js") : "",
+    "MainScript" => $magnify ? file_get_contents("mainscript.min.js") : "",
     "less" => $css,
-    "magcss" => file_get_contents("magnifier.min.css"),
+    "magcss" => $magnify ? file_get_contents("magnifier.min.css") : "",
     "options" => $options,
-    "show_3d" => $show3d
+    "show_3d" => $show3d,
+    "magnify" => $magnify
     );
 $echo = $twig->render('page.html', $vals);
 if ($create) {
